cfb0d6b94160ab32b4bddb5db944e28d7ee6e5b8
Sebastián Jaramillo <108204749+sebastoico@users.noreply.github.com>
2023-12-28 13:39:08 +0000

    Routers merged into one

--- server/api.js
+++ server/api.js
@@ -1,6 +1,67 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const {
+  createMeeting,
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+  deleteAllFromDatabase,
+} = require("./db.js");
 
+// Parameter handler
+apiRouter.use('/:route/:id', (req, res, next) => {
+  const elem = getFromDatabaseById(req.params.route, req.params.id);
+  req.route = req.params.route;
+  req.id = req.params.id;
+  req.elem = elem;
+  if (elem && (req.params.route === "minions" || req.params.route === "ideas")) {
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+// Route methods
+apiRouter.get('/:route/', (req, res, next) => {
+  const data = getAllFromDatabase(req.params.route);
+  res.send(data);
+});
+
+apiRouter.post('/:route', (req, res, next) => {
+  let data;
+  if (req.params.route === "minions" || req.params.route === "ideas") {
+    data = addToDatabase(req.params.route, req.body);
+  } else if (req.params.route === "meetings") {
+    const meeting = createMeeting();
+    data = addToDatabase(req.params.route, meeting);
+  }
+  res.status(201).send(data);
+});
+
+apiRouter.delete('/:route', (req, res, next) => {
+  if (req.params.route === "meetings") {
+    deleteAllFromDatabase(req.params.route);
+    res.status(204).send();
+  }
+})
+
+// Id methods
+apiRouter.get('/:route/:id', (req, res, next) => {
+  const elem = getFromDatabaseById(req.params.route, req.params.id);
+  res.send(elem);
+});
+
+apiRouter.put('/:route/:id', (req, res, next) => {
+  const elem = updateInstanceInDatabase(req.params.route, req.body);
+  res.send(elem);
+});
+
+apiRouter.delete('/:route/:id', (req, res, next) => {
+  deleteFromDatabasebyId(req.params.route, req.id);
+  res.status(204).send();
+});
 
 module.exports = apiRouter;
